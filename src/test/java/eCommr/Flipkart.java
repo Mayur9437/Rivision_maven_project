d30bad01dc914030f3bf9d34b1d2f564bd914aa3
Mayur9437 <msalunke9437@gmail.com>
2023-08-01 18:48:54 +0530

    [Mayur] 2nd commit

---
+++ src/test/java/eCommr/Flipkart.java
@@ -0,0 +1,18 @@
+package eCommr;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+  @Test
+  public void Flipkart_Test() 
+  {
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.amazon.in/");
+	  driver.manage().window().maximize();
+	  driver.manage().deleteAllCookies();
+	  driver.close();
+	  
+  }
+}
