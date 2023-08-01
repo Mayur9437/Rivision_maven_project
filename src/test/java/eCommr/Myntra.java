7ce60db8af7ad1bc4e97c5c843be20182db84451
Mayur9437 <msalunke9437@gmail.com>
2023-08-01 16:33:26 +0530

    [MAYUR] 1st commit

---
+++ src/test/java/eCommr/Myntra.java
@@ -0,0 +1,19 @@
+package eCommr;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Myntra {
+  @Test
+  public void MyntraTest() 
+  {
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.myntra.com/");
+	  driver.manage().window().maximize();
+	  driver.manage().deleteAllCookies();
+	  driver.close();
+	
+  }
+}
